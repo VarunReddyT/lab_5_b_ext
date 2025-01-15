159dcb327cc4a48de74d2213871097b6d294990f
VarunReddyT <tvarun2014@gmail.com>
2025-01-15 19:30:44 +0530

    First

---
+++ src/main/java/maven_java/labfiveb_external/App.java
@@ -0,0 +1,10 @@
+package maven_java.labfiveb_external;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is a sample for SE Lab external");
+    }
+}
